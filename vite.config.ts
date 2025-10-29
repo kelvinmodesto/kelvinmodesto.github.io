99d325f1c991d88829f9508cfe58786d59c67a88
Kelvin Modesto <km.modesto@gmail.com>
2025-10-29 18:06:03 -0300

    feat(tailwind): add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
+/// <reference types="vitest" />
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    css: true,
   },
 });
